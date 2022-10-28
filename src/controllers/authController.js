a975e222e4e3c7198bb9005b48475ce5f7f00650
Eric <menaericdaniel@gmail.com>
2022-10-27 21:00:29 -0300

    signIn & signUp done!

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,69 +1,113 @@
-const {hashSync} =require('bcryptjs');
-const db = require('../database/models');
-const {sendSequelizeError} = require('../helpers');
-const {sign} = require('jsonwebtoken');
+const { hashSync, compareSync } = require("bcryptjs");
+const db = require("../database/models");
+const { sendSequelizeError, createError } = require("../helpers");
+const { sign } = require("jsonwebtoken");
 
 module.exports = {
-    processRegister : async (req,res) => {
-       try {
-            const {name, surname, email, password} = req.body;
+    signUp: async (req, res) => {
+        try {
+            const {
+                name,
+                surname,
+                email,
+                password,
+                street,
+                city,
+                province,
+                birthday,
+            } = req.body;
 
-            const {id, rolId} = await db.User.create({
+            const { id, rolId } = await db.User.create({
                 name: name && name.trim(),
                 surname: surname && surname.trim(),
-                email : email && email.trim(),
-                password : password && password.trim(),
-                rolId : 2
+                email: email && email.trim(),
+                password: password && password.trim(),
+                birthday: birthday && birthday,
+                avatar: req.file && req.file.filename,
+                rolId: 2,
+            });
+
+            await db.Address.create({
+                street: street ? street.trim() : null,
+                city: city ? city.trim() : null,
+                province: province ? province.trim() : null,
+                userId: id,
             });
 
             const token = sign(
                 {
-                    id, 
-                    rolId
+                    id,
+                    rolId,
                 },
-                    process.env.SECRET_KEY_JWT,
+                process.env.SECRET_KEY_JWT,
                 {
-                    expiresIn : '1h'
+                    expiresIn: "1h",
                 }
-            )
+            );
 
             return res.status(201).json({
-                ok : true,
-                status : 201,
-                data : token
-            })
-
-       } catch (error) {
-            
-        let errors = sendSequelizeError(error);
+                ok: true,
+                status: 201,
+                data: token,
+            });
+        } catch (error) {
+            let errors = sendSequelizeError(error);
             return res.status(error.status || 500).json({
-                ok : false,
-                errors 
-            })
-       }
-    },
-    processLogin : (req,res) => {
-        let errors = validationResult(req);
-        if(errors.isEmpty()){
-
-            let {id, name, avatar} = loadUsers().find(user => user.email === req.body.email);
-
-            req.session.userLogin = {
-                id,
-                name,
-                avatar
-            };
-
-            req.body.remember && res.cookie('mercadoLiebre15',req.session.userLogin, {maxAge : 1000 * 60})
-
-
-            return res.redirect('/');
-
-        }else {
-            return res.render('userLogin',{
-                errors : errors.mapped(),
-                old : req.body
-            })
+                ok: false,
+                errors,
+            });
         }
-    }
-}
+    },
+    signIn: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+
+            if(!email || !password) {
+                throw createError(404, 'Se require email y password');
+            }
+
+            let user = await db.User.findOne({
+                where : {
+                    email
+                }
+            });
+
+          /*   if(!user){
+                throw createError(401, 'El usuario no se encuentra registrado');
+            }
+
+            if(!compareSync(password, user.password)){
+                throw createError(401, 'La contraseña es incorrecta');
+            } */
+
+            if(!user || !compareSync(password, user.password)){
+                throw createError(401, 'Credenciales inválidas');
+            }
+
+            const token = sign(
+                {
+                    id : user.id,
+                    rolId : user.rolId,
+                },
+                process.env.SECRET_KEY_JWT,
+                {
+                    expiresIn: "1h",
+                }
+            );
+
+            return res.status(200).json({
+                ok: true,
+                status: 200,
+                data: token,
+            });
+
+
+        } catch (error) {
+            let errors = sendSequelizeError(error);
+            return res.status(error.status || 500).json({
+                ok: false,
+                errors,
+            });
+        }
+    },
+};
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,10 +1,13 @@
 'use strict';
-const {hashSync} = require('bcryptjs')
-const {validationsModelsDefault,objectValidate} =require('../../helpers')
+const fs = require('fs');
+const {hashSync} = require('bcryptjs');
+const moment = require('moment');
+const {validationsModelsDefault,objectValidate, createError} =require('../../helpers')
 
 const {
   Model
 } = require('sequelize');
+const path = require('path');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
 
@@ -47,7 +50,6 @@ module.exports = (sequelize, DataTypes) => {
       type : DataTypes.STRING,
       allowNull : false,
       validate : {
-        
         is : objectValidate(/^[a-zA-Z]+$/i,"Solo se aceptan caracteres alfabéticos"),
         ...validationsModelsDefault,
       }
@@ -70,9 +72,7 @@ module.exports = (sequelize, DataTypes) => {
         async email(value){
             const user = await this.emailVerify(value);
             if(user){
-              let error = new Error('El email ya se encuentra registrado')
-              error.status = 404
-              throw error
+              throw createError(404, "El email ya se encuentra registrado")
             }
         }
       }
@@ -82,7 +82,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull : false,
       validate : {
         ...validationsModelsDefault,
-        //is : objectValidate(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,12}$/,'La contraseña debe tener un mayúscula, una minuscula, un caracter especial y entre 6 y 12 caracteres')
+        is : objectValidate(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,12}$/,'La contraseña debe tener un mayúscula, una minuscula, un caracter especial y entre 6 y 12 caracteres'),
         hashPass(value){
           User.beforeCreate( (user) =>{
             user.password = hashSync(value,10)
@@ -91,8 +91,30 @@ module.exports = (sequelize, DataTypes) => {
     
       }
     },
-    birthday: DataTypes.DATE,
-    avatar : DataTypes.STRING,
+    birthday: {
+      type : DataTypes.DATE,
+      validate : {
+        checkDate(value){
+          if(moment(value).isAfter(moment())){
+            throw createError(404, 'La fecha no puede ser posterior a la actual')
+          } // true
+        }
+      }
+    },
+    avatar : {
+      type : DataTypes.STRING,
+      defaultValue : "default.png",
+      validate : {
+        isImage(value){
+          if(!/.png|.jpg|.jpeg|.webp/i.test(value)){
+            
+            fs.unlinkSync(path.join(__dirname,'..','..','..','public','images','users',value))
+
+            throw createError(404, 'El archivo no es un imágen')
+          }
+        }
+      }
+    },
     genderId: {
       type: DataTypes.INTEGER,
       defaultValue : 3
